bdd49a88e6fa4d89f351cacbe36c38d0ba9bd266
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-03-16 00:21:13 -0300

    new migrations

---
+++ Books.Infrastructure/Migrations/20240316031251_news-entitys-relationship.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Books.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class newsentitysrelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // migrationBuilder.AddColumn<Guid>(
+            //     name: "LibraryId",
+            //     table: "Books",
+            //     type: "uuid",
+            //     nullable: false,
+            //     defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Librarys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    Catalogs = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Librarys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Street = table.Column<string>(type: "text", nullable: true),
+                    State = table.Column<string>(type: "text", nullable: true),
+                    ZipCode = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    Number = table.Column<string>(type: "text", nullable: true),
+                    LibraryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Address_Librarys_LibraryId",
+                        column: x => x.LibraryId,
+                        principalTable: "Librarys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            // migrationBuilder.CreateIndex(
+            //     name: "IX_Books_LibraryId",
+            //     table: "Books",
+            //     column: "LibraryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_LibraryId",
+                table: "Address",
+                column: "LibraryId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Librarys_LibraryId",
+                table: "Books",
+                column: "LibraryId",
+                principalTable: "Librarys",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Librarys_LibraryId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Librarys");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_LibraryId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LibraryId",
+                table: "Books");
+        }
+    }
+}
